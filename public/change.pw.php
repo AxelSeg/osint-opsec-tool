07e11f8cfe258de70e12af53965dbaa509bcc4d0
Brendan Jamieson <bjamieson@actrix.co.nz>
2012-11-20 13:00:41 +1300

    added check_strong_password function

--- public/change.pw.php
+++ public/change.pw.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['user'])){
 else{
 
     require('./libs/bcrypt.php');
+    require('./libs/functions.php');
 
     $user = $_SESSION['user'];
     $old_password_entered = $_POST['old_password'];
@@ -22,31 +23,36 @@ else{
         header('Location: index.php');
     }
     else {
-	if($new_password_entered == $new_password_confirm){
-            require_once($_SERVER['DOCUMENT_ROOT'].'/config/db.php');
+        if($new_password_entered == $new_password_confirm){
+ 
+            if(check_strong_password($new_password_entered)){
 
-	    $stmt = $GLOBALS['dbh']->prepare("SELECT password_hashed FROM `opsec_users` WHERE user = :user");
-	    $stmt->execute(array(':user' => $user));
+                require_once($_SERVER['DOCUMENT_ROOT'].'/config/db.php');
 
-            $row = $stmt->fetch();
+	        $stmt = $GLOBALS['dbh']->prepare("SELECT password_hashed FROM `opsec_users` WHERE user = :user");
+	        $stmt->execute(array(':user' => $user));
+
+                $row = $stmt->fetch();
 		                
-            $hashed_password_from_table = $row['password_hashed'];
+                $hashed_password_from_table = $row['password_hashed'];
 			                
-            $bcrypt = new bcrypt(12);
-            $password_correct = $bcrypt->verify($old_password_entered, $hashed_password_from_table);
+                $bcrypt = new bcrypt(12);
+                $password_correct = $bcrypt->verify($old_password_entered, $hashed_password_from_table);
 
-	    if($password_correct){
-	        $hashed_pw = $bcrypt->genHash($new_password_entered);
-                $passwd_stmt = $GLOBALS['dbh']->prepare("UPDATE `opsec_users` SET `password_hashed` = :password_hashed WHERE `user` = :user");
-		$passwd_stmt->execute(array(':password_hashed' => $hashed_pw, ':user' => $user));
-		echo "Password updated successfully.";
+    	        if($password_correct){
+	            $hashed_pw = $bcrypt->genHash($new_password_entered);
+                    $passwd_stmt = $GLOBALS['dbh']->prepare("UPDATE `opsec_users` SET `password_hashed` = :password_hashed WHERE `user` = :user");
+		    $passwd_stmt->execute(array(':password_hashed' => $hashed_pw, ':user' => $user));
+		    echo "Password updated successfully.";
+	        }
+	        else{
+                    die("Old password not correct!");
+	        }
 	    }
-	    else{
-                echo "Old password not correct!";
-	    }
+	    else{die();}
 	}
 	else{
-            echo "New passwords do not match!";
+            die("New passwords do not match!");
 	}
 	echo "<p>Redirecting you...</p>";
 	echo '<meta http-equiv="refresh" content="5;url=http://'; echo gethostname(); echo '/main.php">';
